74f6fa9f5ef6955b28362a3b56a4f1a9eb14f969
Chandan <chandan.ckmodi@gmail.com>
2024-08-22 06:15:59 +0530

    third

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import './../styles/App.css';
 const App = () => {
   
   return (
-    <div>
+    <div id="main">
         {/* Do not remove the main div */}
         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
         
